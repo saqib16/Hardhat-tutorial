247122e69ad7c3a8ab5ef2a6be2a6bbd4b95ecd9
Zaid Ali <zaid.kpr@gmail.com>
2021-10-07 21:47:41 +0500

    added environment variable for Keys in hardhat conig

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.7.3",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`0x${process.env.ROPSTEN_PRIVATE_KEY}`],
+    },
+  },
+};
